12e5559c8e9ef99647370d3d5510e7e8ad994f5d
SuperShadiao <3381949033@qq.com>
2026-06-26 10:16:14 +0800

    fix: 修复错误的快捷键注册方式, 然后让游戏启动的时候载入一次本地模型

--- fabric/src/main/java/com/elfmcys/yesstevemodel/fabric/mixin/OptionsMixin.java
+++ fabric/src/main/java/com/elfmcys/yesstevemodel/fabric/mixin/OptionsMixin.java
@@ -24,10 +24,10 @@ public class OptionsMixin {
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void ysm$injectCustomKeyMappings(CallbackInfo ci) {
-        List<KeyMapping> all = new ArrayList<>(java.util.Arrays.asList(this.keyMappings));
-        for (KeyMapping km : KeyMappingRegistry.getCustomKeyMappings()) {
-            all.add(km);
-        }
-        this.keyMappings = all.toArray(new KeyMapping[0]);
+//        List<KeyMapping> all = new ArrayList<>(java.util.Arrays.asList(this.keyMappings));
+//        for (KeyMapping km : KeyMappingRegistry.getCustomKeyMappings()) {
+//            all.add(km);
+//        }
+//        this.keyMappings = all.toArray(new KeyMapping[0]);
     }
 }
